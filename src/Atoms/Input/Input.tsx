fc7976ff3be2fb07bb123fda6eea46538a9aca09
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 14:33:00 +0200

    * Input component created

---
+++ src/Atoms/Input/Input.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+
+export const Input = styled.input`
+  width: 350px;
+  height: 40px;
+  background: #292A2B 0 0 no-repeat padding-box;
+  border: none;
+  opacity: 1;
+  margin :8px;
+  padding: 2px 0 2px 15px;
+  color: white;
+
+`
