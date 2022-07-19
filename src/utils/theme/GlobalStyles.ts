c708c4658207d1c0e1b0cf2e80cd5e6125c902c6
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 20:02:56 +0200

    * hotfix Input, Logo, => width and height use scaling unit instead of pixels
    * GlobalStyle => body use overflow:hidden to remove scroll bars
    
    *index.css and app.css removed since we have GlobalStyles.ts
    
    *Font Family changed to Caterman
    
    * Font saved to assets/fonts

--- src/utils/theme/GlobalStyles.ts
+++ src/utils/theme/GlobalStyles.ts
@@ -1,16 +1,16 @@
 import {createGlobalStyle} from "styled-components";
-
-
-
+import  '../../assets/fonts/Catamaran/Catamaran-VariableFont_wght.ttf'
 export const GlobalStyle = createGlobalStyle`
-  
-  
 body{
   margin:0 auto;
   padding:0;
   box-sizing: border-box;
   background-color: #222224;
   overflow: hidden;
+}
+  
+html{
+  font-family: Caterman,sans-serif;
 }
 
 `
