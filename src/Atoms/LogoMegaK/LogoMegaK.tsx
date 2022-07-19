f1e67e06d92150503ae7d79ad049926503d1f20d
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 13:54:35 +0200

    * HomePage Logo component created

---
+++ src/Atoms/LogoMegaK/LogoMegaK.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Logo from '../../assets/images/LogoMegaK.png'
+
+export const LogoMegaK = styled.div`
+
+  background-image: url(${Logo});
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin-bottom: 30px;
+  opacity: 1;
+  width: 124px;
+  height: 76px;
+  
+`
