4a0ce7be683a56db4aee575ece036cbf68872486
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 14:33:14 +0200

    * Wrapper component created

---
+++ src/Atoms/Wrapper/Wrapper.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  padding: 10px;
+  min-height: 100vh;
+  background-color: #222224;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  
+  
+`
