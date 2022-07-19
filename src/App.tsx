dc977c104932264b14217c1f9fe14b7ab5f456a1
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 10:04:42 +0200

    * feature/HomePage branch created

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import {Wrapper} from "./Atoms/Wrapper/Wrapper";
 
-import {ThemeProvider} from "styled-components";
-
 function App() {
   return (
       <>
