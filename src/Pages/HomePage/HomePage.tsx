30f8f8d1d28dc24d0bb9251c3b4b7b1b0c8f319c
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 13:53:55 +0200

    * HomePage Component created which implements HomePageTemplate

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {HomePageTemplate} from "../../Templates/HomePageTemplate/HomePageTemplate";
+
+export const HomePage = () => {
+
+    return (
+        <HomePageTemplate/>
+    )
+}
