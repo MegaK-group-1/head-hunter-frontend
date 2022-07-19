ddd4d91e713546c830031e7e37b46a15b7fbf4ec
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-19 08:29:37 +0200

    * Theme provider added * Basic atomic design folder structure added

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from "styled-components";
+import {GlobalStyle} from "./utils/theme/GlobalStyles";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <ThemeProvider theme={GlobalStyle}/>
     <App />
   </React.StrictMode>
 );
