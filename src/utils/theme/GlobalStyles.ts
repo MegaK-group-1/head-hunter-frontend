65a4d3d09502461fcb2a2f86d4f4f4d24f51e3c9
Grzegorz Strzelecki <grzegorz.strzelecki84@gmail.com>
2022-07-21 20:01:14 +0200

    Fixed global css style reset.

--- src/utils/theme/GlobalStyles.ts
+++ src/utils/theme/GlobalStyles.ts
@@ -1,16 +1,19 @@
 import {createGlobalStyle} from "styled-components";
 import  '../../assets/fonts/Catamaran/Catamaran-VariableFont_wght.ttf'
 export const GlobalStyle = createGlobalStyle`
-body{
-  margin:0 auto;
-  padding:0;
+
+* {
+  padding: 0;
+  margin: 0;
   box-sizing: border-box;
+  font-family: Caterman,sans-serif;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
   background-color: #222224;
   overflow: hidden;
-}
-  
-html{
-  font-family: Caterman,sans-serif;
 }
 
 `
