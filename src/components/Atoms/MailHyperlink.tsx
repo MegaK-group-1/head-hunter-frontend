6c4204c92079ac3861315319b25b640018738ac4
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-23 12:16:00 +0200

    * Atom => hyperlink with e-mail icon

---
+++ src/components/Atoms/MailHyperlink.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { IoMailSharp } from "react-icons/io5";
+import { Hyperlink } from "./Hyperlink";
+
+interface Props {
+  children: string;
+  href: string;
+}
+
+const StyledHyperLink = styled(Hyperlink)`
+  color: #f7f7f7;
+  justify-content: flex-start;
+`;
+
+const MailIcon = styled(IoMailSharp)`
+  color: #4d4d4d;
+`;
+
+export const MailHyperlink = (props: Props) => {
+  const { children, href } = props;
+
+  return (
+    <StyledHyperLink href={`mailto:${href}`}>
+      <MailIcon />
+      {children}
+    </StyledHyperLink>
+  );
+};
