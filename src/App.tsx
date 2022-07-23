7b056e26f49d103b9e710fe7024396f779275b9c
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-23 12:05:18 +0200

    Route for Page-Cv added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
-import {LoginPage} from "./Pages/LoginPage/LoginPage";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from './Pages/LoginPage/LoginPage'
+import { CvPage } from './Pages/CvPage'
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path={'/'} element={<LoginPage/>} />
-
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path={'/'} element={<LoginPage />} />
+        <Route path={'/cv'} element={<CvPage />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
