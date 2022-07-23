d6ae40ba01753142da3d98535ca0487792869cfe
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-23 12:20:54 +0200

    * Organism => Cv organism

---
+++ src/components/Organism/CvOrganism.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Wrapper } from '../Atoms/Wrapper'
+import { CvContactSection } from '../Molecules/CvContactSection/CvContactSection'
+
+export const CvOrganism = () => {
+  return (
+    <Wrapper>
+      <CvContactSection />
+    </Wrapper>
+  )
+}
