51ac16febfb279cea8f8c66f511f93d45b77b4cf
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-23 12:11:47 +0200

    * Atom Avatar added * possible to use in feature on page for HR ( without big prop  )

---
+++ src/components/Atoms/Avatar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {
+    src: string
+    big?: boolean
+}
+
+const Img = styled.img<Props>`
+  border-radius: 50%;
+  width: 7.5vw;
+  height: 7.5vw;
+  min-width: ${({big}) => (big ? '150px' : '45px')};
+  min-height: ${({big}) => (big ? '150px' : '45px')};
+
+`
+
+export const Avatar = (props: Props) => {
+    const {src, big} = props
+
+    return (
+        <Img src={src} big={big}/>
+    )
+}
