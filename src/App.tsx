716dd245b46c57aaaa93c79fd29604a04ddb82e8
Adamski83 <adamsik83@wp.pl>
2022-07-26 18:47:50 +0200

    first commit with new feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './Pages/LoginPage/LoginPage'
 import { CvPage } from './Pages/CvPage'
+import {AdminPage} from "./Pages/AdminPage/AdminPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path={'/'} element={<LoginPage />} />
         <Route path={'/cv'} element={<CvPage />} />
+        <Route path={'/admin'} element={<AdminPage />} />
       </Routes>
     </>
   )
