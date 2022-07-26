b768bdc5bf5be9f710a731f5676f51193b89a235
Adamski83 <adamsik83@wp.pl>
2022-07-26 18:47:50 +0200

    first commit with new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { LoginPage } from "./Pages/LoginPage/LoginPage";
-import { CvPage } from "./Pages/CvPage";
-import { AvailableStudentsPage } from "./Pages/AvailableStudentsPage/AvailableStudentsPage";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from './Pages/LoginPage/LoginPage'
+import { CvPage } from './Pages/CvPage'
+import {AdminPage} from "./Pages/AdminPage/AdminPage";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<LoginPage />}
-      />
-      <Route
-        path="/cv"
-        element={<CvPage />}
-      />
-      <Route
-        path="/available-students"
-        element={<AvailableStudentsPage />}
-      />
-    </Routes>
-  );
+    <>
+      <Routes>
+        <Route path={'/'} element={<LoginPage />} />
+        <Route path={'/cv'} element={<CvPage />} />
+        <Route path={'/admin'} element={<AdminPage />} />
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
