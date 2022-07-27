220ca09b88e2825116867a96f05e5f139276127c
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 20:21:05 +0200

    * CvPage => EducationSection added

---
+++ src/components/Molecules/CvPageMolecules/EducationSection.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {CvHeadLabel} from "../../Atoms/CvHeadLabel";
+import {HeadTitle} from "../../Atoms/HeadTitle";
+import {CvUnderTitleSection} from "../../Atoms/CvUnderTitleSection";
+import {Paragraph} from "../../Atoms/Paragraph";
+
+
+interface Props {
+    education: string
+}
+
+export const EducationSection = (props:Props) => {
+const {education} = props
+
+    return (
+        <>
+            <CvHeadLabel>
+                <HeadTitle>Edukacja</HeadTitle>
+            </CvHeadLabel>
+            <CvUnderTitleSection>
+                <Paragraph>{education}</Paragraph>
+            </CvUnderTitleSection>
+        </>
+    )
+}
