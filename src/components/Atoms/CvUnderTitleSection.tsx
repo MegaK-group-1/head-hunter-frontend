fe08f7b054ba57d9abfa08ab4d547727b421e2f3
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 20:20:31 +0200

    * CvPage => All under title section container

---
+++ src/components/Atoms/CvUnderTitleSection.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props{
+    children: JSX.Element | JSX.Element[];
+    flexDirection? : string;
+}
+
+const Container = styled.section<Props>`
+    background-color:#222324;
+  width:100%;
+  padding: 15px 25px;
+  min-height: 75px;
+  display: flex;
+  flex-direction: ${({flexDirection})=> flexDirection};
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+export const CvUnderTitleSection = (props:Props)=>{
+
+    const {children,flexDirection} = props;
+
+    return(
+
+        <Container flexDirection ={flexDirection}>
+            {children}
+        </Container>
+    )
+}
