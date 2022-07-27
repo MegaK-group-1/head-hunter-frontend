e69f969453391245ce76e5b7e92e98ffa718c204
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 19:24:00 +0200

    * cleanup and styles updated

---
+++ src/components/Atoms/CvDescription.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Paragraph } from './Paragraph'
+
+export const CvDescription = styled(Paragraph)`
+  text-align: justify;
+  font-weight: normal;
+  line-height: 25px;
+  max-width: 90%;
+`
