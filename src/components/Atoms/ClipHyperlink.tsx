0afa4a47b76af924eacf47a0acab031b9f014eac
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 20:18:39 +0200

    * Hyperlink with clip icon

---
+++ src/components/Atoms/ClipHyperlink.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import {BsPaperclip} from 'react-icons/bs'
+import {Hyperlink} from './Hyperlink'
+
+interface Props {
+    href: string
+    children: string
+}
+
+const StyledHyperLink = styled(Hyperlink)`
+  justify-content: flex-start;
+  margin-bottom: 5px;
+  transition: .2s linear;
+  :hover{
+    color: white;
+  }
+`
+
+const ClipIcon = styled(BsPaperclip)`
+  width: 25px;
+  height: 35px;
+`
+
+export const ClipHyperlink = (props: Props) => {
+    const {children, href} = props
+
+    return (
+        <StyledHyperLink target="_blank" href={href}>
+            <ClipIcon/>
+            {children}
+        </StyledHyperLink>
+    )
+}
