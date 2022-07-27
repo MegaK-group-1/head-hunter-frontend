1ce5eab923e7044b03fdbc68db66099685169f74
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 20:19:59 +0200

    * HeadLabel for Cv sections

---
+++ src/components/Atoms/CvHeadLabel.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export const CvHeadLabel = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #292A2B;
+  height: auto;
+  width: 100%;
+  padding: 14px 26px;
+`
