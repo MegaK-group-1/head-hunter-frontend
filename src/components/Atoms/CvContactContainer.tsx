deb9387d12ba5730f066b4fbb1c360d4eec502c7
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-27 19:20:47 +0200

    * cleanup and rwd options added

--- src/components/Atoms/CvContactContainer.tsx
+++ src/components/Atoms/CvContactContainer.tsx
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
 
-export const Container = styled.section`
+export const CvContactContainer = styled.section`
   min-width: 250px;
-  width: 13vw;
+  max-width: 300px;
+  width: 14vw;
   height: 720px;
   background-color: #292a2b;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   flex-direction: column;
-  padding: 30px;
-  box-sizing: border-box;
   gap: 10px;
+  overflow:hidden;
+  padding: 15px 0 0 0 ;
+  @media(max-width: 800px){
+    width:300px
+  }
 `
