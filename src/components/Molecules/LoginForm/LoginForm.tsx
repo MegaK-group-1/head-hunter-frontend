f9a3ab05ea9d8c34e571fd95850f5b98de2a1ea0
pwgawzynski <pgawzynski.backup@gmail.com>
2022-07-28 09:54:00 +0200

    Login action is done

--- src/components/Molecules/LoginForm/LoginForm.tsx
+++ src/components/Molecules/LoginForm/LoginForm.tsx
@@ -1,9 +1,12 @@
-import React, { FormEvent } from 'react'
+import React, {FormEvent, useState} from 'react'
 import styled from 'styled-components'
-import { Input } from '../../Atoms/Input'
+import {ReusableInput} from '../../Atoms/ReusableInput'
 import { StyledLink } from '../../Atoms/Link'
 import { Button } from '../../Atoms/Button'
 import { Paragraph } from '../../Atoms/Paragraph'
+import {api} from "../../../utils/api/api";
+import { UserLoginReq } from 'types'
+import {NavigateFunction, useNavigate} from "react-router-dom";
 
 const StyledForm = styled.form`
   display: flex;
@@ -12,6 +15,20 @@ const StyledForm = styled.form`
   flex-direction: column;
   max-width: 90%;
   padding: 15px;
+`
+
+const OperationStatusInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 90%;
+  padding: 15px;
+`
+
+const OperationStatusInfoText = styled.p`
+  font-size: 15px;
+  color: #e12735;
 `
 
 const PositionWrapper = styled.div`
@@ -23,16 +40,33 @@ const PositionWrapper = styled.div`
   flex-direction: row;
 `
 
+const handleClickOnLogin = async (data : UserLoginReq, responseInfoSetter: React.Dispatch<React.SetStateAction<string>>, nav: NavigateFunction)=>{
+    const response = JSON.parse(await  api.sendLoginReq(data));
+    response.error && responseInfoSetter("Podane dane są niepoprawne !!!");
+    //TODO Change this path below that it will redirect logged user to its account page.
+    nav('./')
+}
+
 export const LoginForm = () => {
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    console.log('Click test')
   }
-  return (
-    <StyledForm onSubmit={handleSubmit}>
-      <Input type='email' placeholder='E-mail' />
 
-      <Input type='password' autoComplete='on' placeholder='Hasło' />
+  const [ loginData, setLoginData ] = useState({} as UserLoginReq );
+  const [ statusText, setStatusText ] = useState('');
+  const nav = useNavigate();
+
+
+    return (
+      <>
+          <OperationStatusInfo>
+              <OperationStatusInfoText>{statusText}</OperationStatusInfoText>
+          </OperationStatusInfo>
+
+    <StyledForm onSubmit={handleSubmit}>
+      <ReusableInput type='email' placeholder='E-mail'  onChangeHandler = {setLoginData} />
+
+      <ReusableInput type='password' autoComplete='on' placeholder='Hasło' onChangeHandler={setLoginData} />
 
       <StyledLink align='flex-end' to='/password-reset'>
         Zapomniałeś Hasła?
@@ -44,8 +78,9 @@ export const LoginForm = () => {
           Zarejestruj się
         </StyledLink>
 
-        <Button>Zaloguj się</Button>
+        <Button onClick={() => handleClickOnLogin(loginData, setStatusText, nav)}>Zaloguj się</Button>
       </PositionWrapper>
     </StyledForm>
+      </>
   )
 }
