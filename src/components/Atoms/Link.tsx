f44fd9734188b2d657a7c4573242652a83f4401f
PatrykKeska <PatrykKeska4@gmail.com>
2022-07-29 11:52:50 +0200

    * apply linters for test in here

--- src/components/Atoms/Link.tsx
+++ src/components/Atoms/Link.tsx
@@ -1,32 +1,30 @@
+import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 interface Props {
-    children: string;
-    to: string;
-    align: string;
-    decoration?: string
+  children: string;
+  to: string;
+  align: string;
+  decoration?: string;
 }
 
 export const StylesLink = styled(Link)<Props>`
-
   color: white;
-  align-self: ${({align}) => align};
-  text-decoration: ${({decoration}) => decoration || "none"};
+  align-self: ${({ align }) => align};
+  text-decoration: ${({ decoration }) => decoration ?? "none"};
+`;
 
-`
+export function StyledLink(props: Props) {
+  const { decoration, align, to, children } = props;
 
-export const StyledLink = (props: Props) => {
-    const {decoration, align, to, children} = props
-
-    return (
-        <StylesLink
-            decoration={decoration}
-            align={align}
-            to={to}
-        >
-            {children}
-        </StylesLink>
-    )
+  return (
+    <StylesLink
+      decoration={decoration}
+      align={align}
+      to={to}
+    >
+      {children}
+    </StylesLink>
+  );
 }
