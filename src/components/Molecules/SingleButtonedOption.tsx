fcb1f4c3b0eb557b9ae39eb9c38cb40d0408efed
Grzegorz Strzelecki <grzegorz.strzelecki84@gmail.com>
2022-08-01 23:05:59 +0200

    Extracted labeled buttons options to separate component SingleButtonedOption.

---
+++ src/components/Molecules/SingleButtonedOption.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement } from "react";
+import styled from "styled-components"
+
+interface Props {
+  labelName: string;
+  options?: ReactElement[];
+  children: React.ReactNode;
+}
+
+const StyledButtonedElement = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+
+  p.label {
+    color: #F7F7F7;
+  }
+
+  .singleOption {
+    color: #F7F7F7;
+  }
+
+  .singleOption:hover {
+    background: #E02735 0% 0% no-repeat padding-box;
+  }
+
+`
+
+export const SingleButtonedOption = (props: Props) => {
+
+  const { labelName, options } = props;
+    
+  return(
+    
+    <StyledButtonedElement>
+      <p className="label">{labelName}</p>
+      <div>
+        {options}
+      </div>
+    </StyledButtonedElement>
+    
+  )
+      
+};
