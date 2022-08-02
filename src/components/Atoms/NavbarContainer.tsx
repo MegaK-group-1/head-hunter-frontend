e5daf4d05b46c0e236adf4f0f02f5c0a64df7d4d
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-02 12:13:12 +0200

    * Navbar Containers components created

---
+++ src/components/Atoms/NavbarContainer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.section`
+  display: flex;
+  padding: 5px 12vw;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1e1e1f;
+  height: 80px;
+  width: 100vw;
+  margin-bottom: 25px;
+`;
---
+++ src/components/Atoms/NavbarMenuContainer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NavbarMenuContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  min-width: 200px;
+  width: 13vw;
+  padding: 5px;
+  gap: 5px;
+`;
