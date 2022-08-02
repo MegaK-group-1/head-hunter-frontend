3e5502657b16b5424599e874ab7e65ee5d3bcac0
Adamski83 <adamsik83@wp.pl>
2022-08-02 20:08:53 +0200

    Added button to Hr Page

--- src/Pages/AddHR/AddHR.tsx
+++ src/Pages/AddHR/AddHR.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Wrapper} from "../../components/Atoms/Wrapper";
 import {LogoMegaK } from "../../components/Atoms/LogoMegaK";
 import {Input} from "../../components/Atoms/Input";
+import { Button} from '../../components/Atoms/Button'
 
 
 export const AddHR = () =>{
@@ -12,6 +13,7 @@ export const AddHR = () =>{
             <Input type='text' autoComplete='on' placeholder='fullName' />
             <Input type='text' autoComplete='on' placeholder='company' />
             <Input type='number' autoComplete='on' placeholder='maxReservedStudents' min={1} max={999}/>
+            <Button>Dodaj</Button>
 
         </Wrapper>
     )
