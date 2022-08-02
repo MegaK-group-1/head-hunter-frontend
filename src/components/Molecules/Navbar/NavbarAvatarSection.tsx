c04906d3fe88a60d818a37318702d1344d426d1e
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-02 12:15:27 +0200

    * Section with Avatar in navbar created. inclues Avatar, UserName, Arrow Button

---
+++ src/components/Molecules/Navbar/NavbarAvatarSection.tsx
@@ -0,0 +1,31 @@
+import React, { MouseEventHandler } from "react";
+import { Avatar } from "../../Atoms/Avatar";
+import avatar from "../../../assets/images/testAvatar.png";
+import { NavbarMenuContainer } from "../../Atoms/NavbarMenuContainer";
+import { NavArrowButton } from "../../Atoms/NavArrowButton";
+import { UnderTitle } from "../../Atoms/UnderTitle";
+
+interface Props {
+  onClick: MouseEventHandler;
+  isOpen: boolean;
+}
+export function NavbarAvatarSection(props: Props) {
+  const { onClick, isOpen } = props;
+  return (
+    <NavbarMenuContainer>
+      <Avatar src={avatar} />
+      <UnderTitle
+        fontWeight="normal"
+        fontSize="1em"
+        color="white"
+        align="center"
+      >
+        Mateusz Kowalski
+      </UnderTitle>
+      <NavArrowButton
+        isOpen={isOpen}
+        onClick={onClick}
+      />
+    </NavbarMenuContainer>
+  );
+}
