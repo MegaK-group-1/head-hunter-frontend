960fcff413f6328b3e689224ac0ec9769e9c45d7
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-02 13:21:55 +0200

    * Navbar renders modal(burger menu ) when state changed

--- src/components/Molecules/Navbar/Navbar.tsx
+++ src/components/Molecules/Navbar/Navbar.tsx
@@ -2,13 +2,28 @@ import React, { useState } from "react";
 import { NavbarContainer } from "../../Atoms/NavbarContainer";
 import { LogoMegaK } from "../../Atoms/LogoMegaK";
 import { NavbarAvatarSection } from "./NavbarAvatarSection";
+import { NavModal } from "./NavModal";
 
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const handleModal = () => {
     setIsOpen(!isOpen);
   };
-  return (
+  return isOpen ? (
+    <>
+      <NavbarContainer>
+        <LogoMegaK
+          height="55px"
+          width="89px"
+        />
+        <NavbarAvatarSection
+          isOpen={isOpen}
+          onClick={handleModal}
+        />
+      </NavbarContainer>
+      <NavModal />
+    </>
+  ) : (
     <NavbarContainer>
       <LogoMegaK
         height="55px"
