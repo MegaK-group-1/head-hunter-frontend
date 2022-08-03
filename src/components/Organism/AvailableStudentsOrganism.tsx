b1067c007f761d8c81491678717fb7af46535ce2
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-03 22:56:51 +0200

    * FakeDb implemented for rendering purpose ( fake studenents)

--- src/components/Organism/AvailableStudentsOrganism.tsx
+++ src/components/Organism/AvailableStudentsOrganism.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Wrapper } from "../Atoms/Wrapper";
 import { StudentsViewWrapper } from "../Atoms/StudentsViewWrapper";
 import { SingleStudentContainer } from "../Molecules/AvailableStudents/SingleStudentContainer";
+import { StudentsUnderNav } from "../Molecules/AvailableStudents/StudentsUnderNav";
+import { fakeDb } from "../../utils/fakeDb";
+import { StudentsFilterHead } from "../Molecules/AvailableStudents/StudentsFilterHead";
 
 export function AvailableStudentsOrganism() {
+  const [students] = useState(fakeDb);
   return (
     <Wrapper verticalStart="flex-start">
       <StudentsViewWrapper>
-        <SingleStudentContainer />
-        <SingleStudentContainer />
-        <SingleStudentContainer />
-        <SingleStudentContainer />
-        <SingleStudentContainer />
-        <SingleStudentContainer />
+        <StudentsUnderNav />
+        <StudentsFilterHead />
+        {students.map((details) => (
+          <SingleStudentContainer
+            key={details.id}
+            details={details}
+          />
+        ))}
       </StudentsViewWrapper>
     </Wrapper>
   );
