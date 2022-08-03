0f373fe16d82604d32674a73f6ddc793584ad0cd
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-03 22:58:39 +0200

    * Types for fakeDB created and also all details are passed by props

--- src/components/Molecules/AvailableStudents/SingleStudentContainer.tsx
+++ src/components/Molecules/AvailableStudents/SingleStudentContainer.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Button, ButtonSize } from "../../Atoms/Button";
 import { StudentsArrowButton } from "../../Atoms/StudentsArrowButton";
 import { UnderTitle } from "../../Atoms/UnderTitle";
 import { StudentsHeadPart } from "../../Atoms/StudentsHeadPart";
-import { cvDetails, FinalCvInterface } from "../../../utils/TestUser";
 import { StudentsExpectationSection } from "../../Atoms/StudentsExpectationSection";
 
 interface Props {
   children?: string;
+  details: {
+    name: string;
+    workPlacePreffer: string;
+    prefferCity: string;
+    expectedContractType: string;
+    expectedSalary: string;
+    consentForFreeInternships: string;
+    commercialProgrammingExperience: string;
+    id: number;
+  };
 }
 
 const Positioner = styled.div`
@@ -18,28 +27,36 @@ const Positioner = styled.div`
   flex-direction: row;
   padding: 10px;
   gap: 25px;
+  @media (max-width: 700px) {
+    font-size: 0.7em;
+    width: 70%;
+    gap: 5px;
+  }
 `;
 
 export function SingleStudentContainer(props: Props) {
-  const [userDetails] = useState(cvDetails as FinalCvInterface);
+  const { details, children } = props;
+  const [userDetails] = useState(details);
   const [isOpen, setOpen] = useState(false);
+
   const handleOpening = () => {
     setOpen(false);
     setOpen(!isOpen);
   };
-  useEffect(() => {}, [isOpen]);
 
-  const { children } = props;
   return (
     <>
-      <StudentsHeadPart isOpen={isOpen}>
+      <StudentsHeadPart
+        key={details.id}
+        isOpen={isOpen}
+      >
         <Positioner>
           <UnderTitle color="white">{children}</UnderTitle>
           <UnderTitle
             fontWeight="normal"
             color="white"
           >
-            Jan Nowak
+            {details.name}
           </UnderTitle>
         </Positioner>
         <Positioner>
@@ -53,7 +70,7 @@ export function SingleStudentContainer(props: Props) {
       {isOpen && (
         <StudentsExpectationSection
           isOpen={isOpen}
-          userDetails={userDetails.expectations}
+          userDetails={userDetails}
         />
       )}
     </>
