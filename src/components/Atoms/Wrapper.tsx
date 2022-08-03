0e4d6e5a5fda5891600dbf9d3b13dc39664bc2b6
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-03 12:07:25 +0200

    * main Wrapper component right now can use props for setting flexbox vertical positioning

--- src/components/Atoms/Wrapper.tsx
+++ src/components/Atoms/Wrapper.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+interface Props {
+  verticalStart?: string;
+}
+
+export const Wrapper = styled.div<Props>`
   width: 100vw;
   min-height: 100vh;
   background-color: #222224;
   display: flex;
-  justify-content: center;
+  justify-content: ${({ verticalStart }) => verticalStart || "center"};
   align-items: center;
   flex-direction: column;
   overflow: hidden;
