8624b888a132d99a8e98656d1799b9c802d04409
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-08 20:55:40 +0200

    * basic setup of students expectation section

--- src/components/Atoms/StudentsExpectationSection.tsx
+++ src/components/Atoms/StudentsExpectationSection.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { CvExpectations } from "../../utils/TestUser";
 import { CvUnderTitleSection } from "./CvUnderTitleSection";
 import { GridContainer } from "./GridContainer";
 import { CvFlexContainer } from "./CvFlexContainer";
 import { UnderTitle } from "./UnderTitle";
+import { StudentDetails } from "../../utils/types/user";
 
 interface ExpectationsDetails {
-  userDetails: CvExpectations;
+  userDetails: StudentDetails;
   isOpen?: boolean;
 }
 
@@ -17,17 +17,25 @@ enum UnderTitleOptions {
   salary = "Oczekiwane wynagrodzenie miesięczne netto",
   trial = "Zgoda na odbycie bezpłatnych praktyk/stażu na początek",
   experince = "Komercyjne doświadczenie w programowaniu",
+  courseCompletion = "Ocena przejścia kursu",
+  courseEngagment = "Ocena aktywności i zaangażowania na kursie",
+  projectDegree = "Ocena kodu w projekcie własnym",
+  teamProjectDegree = "Ocena pracy w zespole w Scrum",
 }
 
 export function StudentsExpectationSection(props: ExpectationsDetails) {
   const { userDetails, isOpen } = props;
   const {
-    workPlacePreffer,
-    prefferCity,
+    canTakeApprenticeship,
+    expectedTypeWork,
     expectedContractType,
+    monthsOfCommercialExp,
+    targetWorkCity,
     expectedSalary,
-    consentForFreeInternships,
-    commercialProgrammingExperience,
+    teamProjectDegree,
+    courseEngagment,
+    courseCompletion,
+    projectDegree,
   } = userDetails;
 
   return (
@@ -39,73 +47,171 @@ export function StudentsExpectationSection(props: ExpectationsDetails) {
         <CvFlexContainer borderR>
           <UnderTitle
             fontWeight="normal"
+            fontSize=".7em"
+            color="#CFCFCF"
+            height="30px"
+          >
+            {UnderTitleOptions.courseCompletion}
+          </UnderTitle>
+          <UnderTitle
             fontSize=".8em"
+            fontWeight="normal"
+          >
+            {courseCompletion} / 5
+          </UnderTitle>
+        </CvFlexContainer>
+
+        <CvFlexContainer borderR>
+          <UnderTitle
+            fontWeight="normal"
+            fontSize=".7em"
+            color="#CFCFCF"
+            height="30px"
+          >
+            {UnderTitleOptions.courseEngagment}
+          </UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {courseEngagment} / 5
+          </UnderTitle>
+        </CvFlexContainer>
+
+        <CvFlexContainer borderR>
+          <UnderTitle
+            fontWeight="normal"
+            fontSize=".7em"
+            color="#CFCFCF"
+            height="30px"
+          >
+            {UnderTitleOptions.projectDegree}
+          </UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {projectDegree} / 5
+          </UnderTitle>
+        </CvFlexContainer>
+
+        <CvFlexContainer borderR>
+          <UnderTitle
+            fontWeight="normal"
+            fontSize=".7em"
+            color="#CFCFCF"
+            height="30px"
+          >
+            {UnderTitleOptions.teamProjectDegree}
+          </UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {teamProjectDegree} / 5
+          </UnderTitle>
+        </CvFlexContainer>
+
+        <CvFlexContainer borderR>
+          <UnderTitle
+            fontWeight="normal"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.location}
           </UnderTitle>
-          <UnderTitle>{workPlacePreffer}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {expectedTypeWork}
+          </UnderTitle>
         </CvFlexContainer>
 
         <CvFlexContainer borderR>
           <UnderTitle
             fontWeight="normal"
-            fontSize=".8em"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.city}
           </UnderTitle>
-          <UnderTitle>{prefferCity}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {targetWorkCity}
+          </UnderTitle>
         </CvFlexContainer>
 
         <CvFlexContainer borderR>
           <UnderTitle
             fontWeight="normal"
-            fontSize=".8em"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.contract}
           </UnderTitle>
-          <UnderTitle>{expectedContractType}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {expectedContractType}
+          </UnderTitle>
         </CvFlexContainer>
 
         <CvFlexContainer borderR>
           <UnderTitle
             fontWeight="normal"
-            fontSize=".8em"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.salary}
           </UnderTitle>
-          <UnderTitle>{expectedSalary}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {expectedSalary}
+          </UnderTitle>
         </CvFlexContainer>
 
         <CvFlexContainer borderR>
           <UnderTitle
             fontWeight="normal"
-            fontSize=".8em"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.trial}
           </UnderTitle>
-          <UnderTitle>{consentForFreeInternships}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {canTakeApprenticeship}
+          </UnderTitle>
         </CvFlexContainer>
 
-        <CvFlexContainer borderR>
+        <CvFlexContainer>
           <UnderTitle
             fontWeight="normal"
-            fontSize=".8em"
+            fontSize=".7em"
             color="#CFCFCF"
             height="30px"
           >
             {UnderTitleOptions.experince}
           </UnderTitle>
-          <UnderTitle>{commercialProgrammingExperience}</UnderTitle>
+          <UnderTitle
+            fontSize=".8em"
+            fontWeight="normal"
+          >
+            {monthsOfCommercialExp}
+          </UnderTitle>
         </CvFlexContainer>
       </GridContainer>
     </CvUnderTitleSection>
