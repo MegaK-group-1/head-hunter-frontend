d748fc32e1303b20a5c9f5f7f9033d5f61c2e1f1
oohajo <99666208+oohajo@users.noreply.github.com>
2022-08-08 15:56:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React, {useEffect, useState} from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { LoginPage } from './Pages/LoginPage/LoginPage'
-import { CvPage } from './Pages/CvPage'
-import {UserPage} from "./Pages/UserPage/UserPage"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./Pages/LoginPage/LoginPage";
+import { CvPage } from "./Pages/CvPage";
+import { AdminPage } from "./components/Organism/AdminPage";
+import CsvReader from "./Pages/CsvReader/CsvReader";
+import { AddHR } from "./components/Organism/AddHR";
+import { UserPage } from "./Pages/UserPage/UserPage";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path={'/'} element={<LoginPage />} />
-        <Route path={'/userPage'} element={<UserPage />} />
-        <Route path={'/cv'} element={<CvPage />} />
-      </Routes>
-    </>
-  )
+    return (
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/cv" element={<CvPage />} />
+            <Route path="/admin"  element={<AdminPage />} />
+            <Route path="/csv" element={<CsvReader />} />
+            <Route path="/hr" element={<AddHR />} />
+            <Route path="/userPage" element={<UserPage />} />
+        </Routes>
+    );
 }
 
 export default App
