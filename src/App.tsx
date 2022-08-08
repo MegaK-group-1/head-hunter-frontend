675c025a2f60be7bcf7780f97c6d18c0d8f00f78
oohajo <oohajo@gmail.com>
2022-08-08 20:58:52 +0200

    App component updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
+export default App
+
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./Pages/LoginPage/LoginPage";
 import { CvPage } from "./Pages/CvPage";
-// import { AdminPage } from "./components/Organism/AdminPage";
-// import CsvReader from "./Pages/CsvReader/CsvReader";
-// import { AddHR } from "./components/Organism/AddHR";
+import { AdminPage } from "./components/Organism/AdminPage";
+import CsvReader from "./Pages/CsvReader/CsvReader";
+import { AddHR } from "./components/Organism/AddHR";
 import { UserPage } from "./Pages/UserPage/UserPage";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/cv" element={<CvPage />} />
-            {/*<Route path="/admin"  element={<AdminPage />} />*/}
-            {/*<Route path="/csv" element={<CsvReader />} />*/}
-            {/*<Route path="/hr" element={<AddHR />} />*/}
-            <Route path="/userPage" element={<UserPage />} />
+            <Route
+                path="/"
+                element={<LoginPage />}
+            />
+            <Route
+                path="/cv"
+                element={<CvPage />}
+            />
+            <Route
+                path="/admin"
+                element={<AdminPage />}
+            />
+            <Route
+                path="/csv"
+                element={<CsvReader />}
+            />
+            <Route
+                path="/hr"
+                element={<AddHR />}
+            />
+            <Route
+                path="/userPage"
+                element={<UserPage />}
+            />
         </Routes>
     );
 }
 
-export default App
+export default App;
