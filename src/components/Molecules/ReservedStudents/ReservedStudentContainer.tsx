f3af1fe0124f6e84d9c1595c2bfceb50a11f5d28
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-09 19:26:07 +0200

    * Basic scheme for Reserved Student Container, not contain yet a full view elements.

---
+++ src/components/Molecules/ReservedStudents/ReservedStudentContainer.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Button, ButtonSize } from "../../Atoms/Button";
+import { StudentsArrowButton } from "../../Atoms/StudentsArrowButton";
+import { UnderTitle } from "../../Atoms/UnderTitle";
+import { StudentsHeadPart } from "../../Atoms/StudentsHeadPart";
+import { StudentsExpectationSection } from "../../Atoms/StudentsExpectationSection";
+import { StudentDetails } from "../../../utils/types/user";
+import { CvLink } from "../../Atoms/CvLink";
+
+interface Props {
+  details: StudentDetails;
+}
+
+const Positioner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding: 10px;
+  gap: 25px;
+  @media (max-width: 700px) {
+    font-size: 0.7em;
+    width: 70%;
+    gap: 5px;
+  }
+`;
+
+export function ReservedStudentContainer(props: Props) {
+  const { details } = props;
+  const [userDetails] = useState(details);
+  const [isOpen, setOpen] = useState(false);
+
+  const handleUninterested = () => {
+    // here is going to be fetch for backend that this student is no more reserved
+    return true;
+  };
+  const handleHired = () => {
+    // there will be fetch that tells this person is hired!
+    return true;
+  };
+  const handleOpening = () => {
+    setOpen(false);
+    setOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <StudentsHeadPart
+        key={details.id}
+        isOpen={isOpen}
+      >
+        <Positioner>
+          <UnderTitle
+            fontWeight="normal"
+            color="white"
+          >
+            {`${details.firstName}  ${details.lastName}`}
+          </UnderTitle>
+        </Positioner>
+        <Positioner>
+          <CvLink to={details.id}>Pokaz CV</CvLink>
+          <Button
+            onClick={handleUninterested}
+            size={ButtonSize.small}
+          >
+            Brak zainteresowania
+          </Button>
+          <Button
+            onClick={handleHired}
+            size={ButtonSize.small}
+          >
+            Zatrudniony
+          </Button>
+          <StudentsArrowButton
+            onClick={handleOpening}
+            isOpen={isOpen}
+          />
+        </Positioner>
+      </StudentsHeadPart>
+      {isOpen && (
+        <StudentsExpectationSection
+          isOpen={isOpen}
+          userDetails={userDetails}
+        />
+      )}
+    </>
+  );
+}
