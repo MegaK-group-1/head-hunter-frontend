68c6f1c5821dbfeea37041d05594292cde30be20
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-09 11:20:28 +0200

    * fetching logic, all users from DB which allow us to setState with all users in other file

---
+++ src/components/Molecules/AvailableStudents/functions /getAllStudents.ts
@@ -0,0 +1,43 @@
+import { GetAllUsers, StudentDetails } from "../../../../utils/types/user";
+
+export const getAllStudents = (): Promise<StudentDetails[]> => {
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:3001/users");
+    const data = await response.json();
+    const { users } = data;
+    return users.map((u: GetAllUsers) => {
+      const {
+        courseCompletion,
+        courseEngagment,
+        projectDegree,
+        teamProjectDegree,
+        expectedContractType,
+        targetWorkCity,
+        expectedTypeWork,
+        canTakeApprenticeship,
+        expectedSalary,
+        monthsOfCommercialExp,
+        id,
+      } = u.userDetails;
+      return {
+        id,
+        firstName: u.firstName,
+        lastName: u.lastName,
+        courseCompletion,
+        courseEngagment,
+        projectDegree,
+        teamProjectDegree,
+        expectedTypeWork,
+        targetWorkCity,
+        expectedContractType:
+          (expectedContractType.toString() === "no_matter" && "BRAK") ||
+          expectedContractType,
+        expectedSalary,
+        canTakeApprenticeship: (canTakeApprenticeship === 0 && "TAK") || "NIE",
+        monthsOfCommercialExp:
+          (monthsOfCommercialExp === 0 && "BRAK") || monthsOfCommercialExp,
+      };
+    });
+  };
+  return fetchData();
+};
