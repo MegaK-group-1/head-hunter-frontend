714f91a524f3c6f3509b18bd613910a261bcafcf
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-09 19:21:28 +0200

    * NavLink which looks like button but will move us for exact cv view

---
+++ src/components/Atoms/CvLink.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+interface Props {
+  children: string;
+  width?: string;
+  to: string;
+}
+
+const StyledCvLink = styled(NavLink)<Props>`
+  min-width: 100px;
+  height: 39px;
+  letter-spacing: 2px;
+  background-color: #e12735;
+  color: white;
+  text-align: center;
+  border: none;
+  align-self: center;
+  transition: 0.2s linear;
+  text-decoration: none;
+  padding: 10px;
+  font-size: 0.9em;
+  :hover {
+    box-shadow: 0 0 1px 1px white;
+    cursor: pointer;
+  }
+`;
+
+export function CvLink(props: Props) {
+  const { children, width, to } = props;
+
+  return (
+    <StyledCvLink
+      width={width}
+      to={`/student/:${to}`}
+    >
+      {children}
+    </StyledCvLink>
+  );
+}
