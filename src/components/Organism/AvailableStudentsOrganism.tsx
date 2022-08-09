b9640c27db87e8ff5ec2c895b3513a414fd9b7f3
PatrykKeska <PatrykKeska4@gmail.com>
2022-08-09 11:17:51 +0200

    * fetching data was moved for separate file

--- src/components/Organism/AvailableStudentsOrganism.tsx
+++ src/components/Organism/AvailableStudentsOrganism.tsx
@@ -3,45 +3,27 @@ import { Wrapper } from "../Atoms/Wrapper";
 import { StudentsViewWrapper } from "../Atoms/StudentsViewWrapper";
 import { StudentsUnderNav } from "../Molecules/AvailableStudents/StudentsUnderNav";
 import { StudentsFilterHead } from "../Molecules/AvailableStudents/StudentsFilterHead";
-import { GetAllUsers, StudentDetails } from "../../utils/types/user";
+import { StudentDetails } from "../../utils/types/user";
 import { SingleStudentContainer } from "../Molecules/AvailableStudents/SingleStudentContainer";
+import { getAllStudents } from "../Molecules/AvailableStudents/functions /getAllStudents";
 
 export function AvailableStudentsOrganism() {
   const [students, setStudents] = useState([] as StudentDetails[]);
   useEffect(() => {
     (async () => {
-      const response = await fetch("http://localhost:3001/users");
-      const data = await response.json();
-      const { users } = data;
-      const studentFilter = users.map((u: GetAllUsers) => {
-        return {
-          id: u.id,
-          firstName: u.firstName,
-          lastName: u.lastName,
-          courseCompletion: u.userDetails.courseCompletion,
-          courseEngagment: u.userDetails.courseEngagment,
-          projectDegree: u.userDetails.projectDegree,
-          teamProjectDegree: u.userDetails.projectDegree,
-          expectedTypeWork: u.userDetails.expectedTypeWork,
-          targetWorkCity: u.userDetails.targetWorkCity,
-          expectedContractType: u.userDetails.expectedContractType,
-          expectedSalary: u.userDetails.expectedSalary,
-          canTakeApprenticeship:
-            u.userDetails.canTakeApprenticeship === 0 ? "NIE" : "TAK",
-          monthsOfCommercialExp: u.userDetails.monthsOfCommercialExp,
-        };
-      });
-      setStudents(studentFilter);
+      setStudents(await getAllStudents());
     })();
   }, []);
-
   return (
     <Wrapper verticalStart="flex-start">
       <StudentsViewWrapper>
         <StudentsUnderNav />
         <StudentsFilterHead />
         {students.map((single) => (
-          <SingleStudentContainer details={single} />
+          <SingleStudentContainer
+            key={single.id}
+            details={single}
+          />
         ))}
       </StudentsViewWrapper>
     </Wrapper>
