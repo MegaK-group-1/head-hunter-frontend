d611f7737984fb168de67d962362f4b7a040f1f7
Grzegorz Strzelecki <grzegorz.strzelecki84@gmail.com>
2022-08-28 17:08:22 +0200

    Changed main app files to see modal working - for demo purposes. Added tab in line code for Paragraph.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
-import {LoginPage} from "./Pages/LoginPage/LoginPage";
+import React,  { useState } from 'react';
+import { Modal } from './components/Organism/Modal';
+import styled from 'styled-components';
 
-function App() {
-    return (
-        <>
-            <Routes>
-                <Route path={'/'} element={<LoginPage/>} />
+const StyledApp = styled.div`
 
-            </Routes>
-        </>
-    );
+  .App {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+  }
+
+  `
+
+export const App = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <StyledApp>
+      <div className="App">
+          <h1>Naciśnij przycisk by otworzyć modal filtracji.</h1>
+          <button className="openModalBtn" onClick={() => setOpenModal(true)}>Otwórz modal</button>
+          {openModal && <Modal closeModal={setOpenModal}/> }
+        </div>
+    </StyledApp>
+  );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter} from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import {GlobalStyle} from "./utils/theme/GlobalStyles";
 
